4e476fb8df1380029dcc7c0a309a0732df6461f9
xudafeng <xudafeng@126.com>
2015-09-04 17:50:55 +0800

    add fastjson

--- android_app_bootstrap/src/main/java/com/github/android_app_bootstrap/MainActivity.java
+++ android_app_bootstrap/src/main/java/com/github/android_app_bootstrap/MainActivity.java
@@ -7,9 +7,9 @@ import android.os.Bundle;
 @SuppressLint("SetJavaScriptEnabled")
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
 }
