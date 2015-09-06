3c216c9a30206a52dc74157833e45d5000b46773
xudafeng <xudafeng@126.com>
2015-09-06 14:34:29 +0800

    tweak files

---
+++ android_app_bootstrap/src/main/java/com/github/android_app_bootstrap/fragment/HomeFragment.java
@@ -0,0 +1,41 @@
+package com.github.android_app_bootstrap.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import com.github.android_app_bootstrap.R;
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.home_fragment, container, false);
+        initView(view);
+        return view;
+    }
+
+    private void initView(View view) {
+
+        TextView titleTextView = (TextView) view.findViewById(R.id.title_text);
+
+        titleTextView.setText("首页");
+
+        WebView webView = (WebView) view.findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message,
+                                     JsResult result) {
+                return super.onJsAlert(view, url, message, result);
+            }
+        });
+        webView.loadUrl("http://www.baidu.com");
+    }
+}
