9aa53067e0950612a4d100e66d5959e96f42546b
xudafeng <xudafeng@126.com>
2016-04-13 16:00:26 +0800

    add list

---
+++ android_app_bootstrap/src/main/java/com/github/android_app_bootstrap/activity/TableActivity.java
@@ -0,0 +1,48 @@
+package com.github.android_app_bootstrap.activity;
+
+/**
+ * Created by xdf on 10/8/15.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.github.android_app_bootstrap.R;
+import com.github.android_app_bootstrap.common.Utils;
+
+public class TableActivity extends Activity {
+
+    private static final Utils utils = new Utils();
+
+    private static final String[] list = new String[]{
+            "Toast"
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.table_activity);
+
+        ListView listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, list));
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                switch (arg2) {
+                    case 0:
+                        Intent intent = new Intent(TableActivity.this, ToastActivity.class);
+                        startActivity(intent);
+                }
+            }
+        });
+
+    }
+}
---
+++ android_app_bootstrap/src/main/java/com/github/android_app_bootstrap/activity/ToastActivity.java
@@ -0,0 +1,47 @@
+package com.github.android_app_bootstrap.activity;
+
+/**
+ * Created by xdf on 10/8/15.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.github.android_app_bootstrap.R;
+import com.github.android_app_bootstrap.common.Utils;
+
+public class ToastActivity extends Activity implements View.OnClickListener {
+
+    private final Utils utils = new Utils();
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.toast_activity);
+        initView();
+    }
+
+    public void initView() {
+        Button button = (Button) findViewById(R.id.toast_button);
+        button.setOnClickListener(this);
+    }
+
+    void showToast() {
+        Toast toast = Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP | Gravity.CENTER, -50, 100);
+        toast.show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.toast_button:
+                showToast();
+                break;
+
+        }
+    }
+}
