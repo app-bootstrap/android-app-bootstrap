cc3711cd83afcc2dc03fcc5e90a841853f9a1d74
hanqiongly <772657880@qq.com>
2016-12-15 17:26:10 +0800

    add pinch drag rotate detect
    remove unused gesture detect library , we can use android gesture
    official library.

--- android_app_bootstrap/src/main/java/com/github/android_app_bootstrap/activity/GestureActivity.java
+++ android_app_bootstrap/src/main/java/com/github/android_app_bootstrap/activity/GestureActivity.java
@@ -6,16 +6,22 @@ package com.github.android_app_bootstrap.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.widget.TextView;
 
 import com.github.android_app_bootstrap.R;
 import com.github.android_app_bootstrap.gesture.MyGestureListener;
+import com.github.android_app_bootstrap.gesture.MyScaleGestureListener;
 
 public class GestureActivity extends Activity {
+    private static final String TAG = "testGestActivity";
     private MyGestureListener gestureListener;
+    private MyScaleGestureListener scaleGestureListener;
     private GestureDetector gestureDetector;
+    private ScaleGestureDetector scaleGestureDetector;
     private TextView dispGestureLabel;
 
     @Override
@@ -24,15 +30,21 @@ public class GestureActivity extends Activity {
         setContentView(R.layout.gesture_activity);
         dispGestureLabel = (TextView) findViewById(R.id.text);
         gestureListener = new MyGestureListener(dispGestureLabel);
+        scaleGestureListener = new MyScaleGestureListener(dispGestureLabel);
         gestureDetector = new GestureDetector(this, gestureListener);
+        scaleGestureDetector = new ScaleGestureDetector(this,scaleGestureListener);
     }
+
 
     public boolean onTouchEvent(MotionEvent event) {
         gestureDetector.onTouchEvent(event);
+        scaleGestureDetector.onTouchEvent(event);
         return super.onTouchEvent(event);
     }
 
     public void onDestroy() {
         super.onDestroy();
     }
+
+
 }
--- android_app_bootstrap/src/main/java/com/github/android_app_bootstrap/gesture/MyGestureListener.java
+++ android_app_bootstrap/src/main/java/com/github/android_app_bootstrap/gesture/MyGestureListener.java
@@ -8,51 +8,27 @@ import android.widget.TextView;
  * Created by liuyang on 2016/12/13.
  */
 
-public class MyGestureListener extends GestureDetector.SimpleOnGestureListener{
-    private static final String TAG = "test_Gesture";
+public class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+    private static final String TAG = "test_GestureScroll";
 
     private static final String GESTURE_SINGLE_TAP = "singleTap";
     private static final String GESTURE_DOUBLE_TAP = "doubleTap";
-    private static final String GESTURE_ROTATE = "rotate";
-    private static final String GESTURE_SWIPE = "swipe";
     private static final String GESTURE_LONG_PRESS = "longPress";
-    private static final String GESTURE_PINCH = "pinch";
     private static final String GESTURE_DRAG = "drag";
 
     private TextView gestureLabel;
 
     private String gestureName;
-    private float scale;
-    private float rotate;
     private float axis_x;
     private float axis_y;
+
+    private static final float DRAG_THRESHOLD = 10f;
 
     public MyGestureListener(TextView textView) {
         gestureLabel = textView;
     }
 
-
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
-
-
-
-    public boolean onScroll(MotionEvent e1, MotionEvent e2,
-                            float distanceX, float distanceY) {
-        return false;
-    }
-
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-                           float velocityY) {
-        return false;
-    }
-
-
-
-    //---------------------------  Determined Gesture Callback--------------------
-
-    //单击事件
+    //single tap gesture detect
     public boolean onSingleTapConfirmed(MotionEvent e) {
         gestureName = GESTURE_SINGLE_TAP;
         axis_x = e.getX();
@@ -61,7 +37,7 @@ public class MyGestureListener extends GestureDetector.SimpleOnGestureListener{
         return super.onSingleTapConfirmed(e);
     }
 
-    //双击事件
+    //double tap gesture detect
     public boolean onDoubleTap(MotionEvent e) {
         gestureName = GESTURE_DOUBLE_TAP;
         axis_x = e.getX();
@@ -70,12 +46,25 @@ public class MyGestureListener extends GestureDetector.SimpleOnGestureListener{
         return super.onDoubleTap(e);
     }
 
-    //长按事件
+    //long press gesture detect
     public void onLongPress(MotionEvent e) {
         gestureName = GESTURE_LONG_PRESS;
         axis_x = e.getX();
         axis_y = e.getY();
         gestureLabel.setText(gestureName + " x:" + axis_x + " y:" + axis_y);
+    }
+
+    //Gesture drag detect
+    public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                            float distanceX, float distanceY) {
+        double dist = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+        if (dist > DRAG_THRESHOLD || distanceX > DRAG_THRESHOLD || distanceY > DRAG_THRESHOLD) {
+            gestureName = GESTURE_DRAG;
+            axis_x = e2.getX();
+            axis_y = e2.getY();
+            gestureLabel.setText(gestureName + " x:" + axis_x + " y:" + axis_y);
+        }
+        return false;
     }
 
 
