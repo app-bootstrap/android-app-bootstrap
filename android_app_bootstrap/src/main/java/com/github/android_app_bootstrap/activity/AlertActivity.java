6dabf978200f8b3354570287aac8de7015b8619a
xudafeng <xudafeng@126.com>
2016-12-17 10:29:07 +0800

    add keyevent

--- android_app_bootstrap/src/main/java/com/github/android_app_bootstrap/activity/AlertActivity.java
+++ android_app_bootstrap/src/main/java/com/github/android_app_bootstrap/activity/AlertActivity.java
@@ -7,8 +7,10 @@ package com.github.android_app_bootstrap.activity;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.github.android_app_bootstrap.R;
 import com.github.android_app_bootstrap.common.Utils;
@@ -16,6 +18,7 @@ import com.github.android_app_bootstrap.common.Utils;
 public class AlertActivity extends Activity implements View.OnClickListener {
 
     private final Utils utils = new Utils();
+    private TextView show_keyevent;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +29,7 @@ public class AlertActivity extends Activity implements View.OnClickListener {
     public void initView() {
         Button button = (Button) findViewById(R.id.alert_button);
         button.setOnClickListener(this);
+        show_keyevent = (TextView) findViewById(R.id.show_keyevent);
         showSimpleDialog();
     }
 
@@ -47,5 +51,18 @@ public class AlertActivity extends Activity implements View.OnClickListener {
                 break;
 
         }
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        // TODO Auto-generated method stub
+        final int keyCode = event.getKeyCode();
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                show_keyevent.setText("KeyCode: " + keyCode);
+            }
+        });
+        return super.dispatchKeyEvent(event);
     }
 }
