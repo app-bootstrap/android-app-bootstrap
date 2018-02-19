461fa9165ba952d8cc6d1674923c1221f21f130d
xdf <xudafeng@126.com>
2018-02-19 10:11:30 +0800

    Update macaca-datahub.config.js

--- macaca-datahub.config.js
+++ macaca-datahub.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
   mode: 'local',
   port: 7001,
-  dataHubView: {
+  view: {
     assetsUrl: 'https://npmcdn.com/datahub-view@latest',
   },
   store: path.resolve(__dirname, 'data')
