7ec91a6850583996b9b9a15f7d163b5ffb48202d
xudafeng <xudafeng@126.com>
2018-03-17 21:53:44 +0800

    tuning to whitelist

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const path = require('path');
+
+exports.appPath = path.join(
+  __dirname,
+  'android_app_bootstrap',
+  'build',
+  'outputs',
+  'apk',
+  'android_app_bootstrap-debug.apk'
+);
