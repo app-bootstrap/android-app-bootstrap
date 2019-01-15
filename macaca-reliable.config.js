dee6a1755daf5b04428287851675be87ac604f3f
xudafeng <xudafeng@126.com>
2019-01-16 01:29:31 +0800

    chore: update ci service

--- macaca-reliable.config.js
+++ macaca-reliable.config.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const helper = require('marmot-cli/lib/helper');
+const helper = require('reliable-cli/lib/helper');
 
 const pkg = require('./package');
 
