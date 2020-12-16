950b4c081ee6584e7be0319353c1627e46c9eb2a
xudafeng <xudafeng@126.com>
2020-12-16 20:04:00 +0800

    chore: update deps

--- macaca-datahub.config.js
+++ macaca-datahub.config.js
@@ -5,8 +5,5 @@ const path = require('path');
 module.exports = {
   mode: 'local',
   port: 7001,
-  view: {
-    assetsUrl: 'https://npmcdn.com/datahub-view@latest',
-  },
   store: path.resolve(__dirname, 'data')
 };
